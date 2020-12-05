bc6b53f7678a83116823743e4ef99f72deff073a
Junyi Ji <junyiji@gmail.com>
2020-12-05 10:53:12 +0100

    Allow color HEX to be copied to clipboard when clicking on any listed color, display and hide alert

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../stylesheets/components/color.scss';
 
 const Color = ({ index, rgb, hex, weight }) => {
@@ -6,15 +6,30 @@ const Color = ({ index, rgb, hex, weight }) => {
 	const colorRGB = `rgb(${rgb.join(',')})`;
 	const colorHEX = `#${hex}`
 
+	const [isCopied, setIsCopied] = useState(false);
+
+	const timeout = () => {
+		setTimeout(() => { setIsCopied(false) }, 500);
+	}
+
+	useEffect(timeout, [isCopied]);
+
+	const handleClick = () => {
+		setIsCopied(true);
+		navigator.clipboard.writeText(colorHEX);
+	};
+
 	return (
 		<article 
 			key={index} 
 			index={index} 
 			className={`color ${darkOrLight}`}
-			style={{backgroundColor: colorRGB}}
+			style={{backgroundColor: colorRGB}} 
+			onClick={handleClick}
 		>
 			<p>{colorHEX}</p>
 			<p>{weight}%</p>
+			{ isCopied && <p className="color_copy-message">Copied to clipboard</p> }
 		</article>
 	)
 };
